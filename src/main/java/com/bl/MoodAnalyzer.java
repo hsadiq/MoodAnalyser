3ec03e69405bdd888f7c249f5a050bfa55d9f30d
hsadiq <ksadik320@gmail.com>
2023-05-08 12:16:54 +0530

    Adding Enum for checking NUll or Empty Mood

--- src/main/java/com/bl/MoodAnalyzer.java
+++ src/main/java/com/bl/MoodAnalyzer.java
@@ -3,35 +3,85 @@ package com.bl;
 public class MoodAnalyzer {
 
     private String message;
-
-    public MoodAnalyzer() {
-        this.message = "";
-    }
-
-    public MoodAnalyzer(String message) {
+    public MoodAnalyzer(String message){
         this.message = message;
     }
 
-    public String moodAnalyze() {
-        if (this.message.trim().toLowerCase().indexOf("sad") != -1) {
+
+    public String moodAnalyze() throws MoodAnalysisException {
+        if (this.message == null){
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.NULL_MOOD, "Mood cannot be null");
+        }
+        if (this.message.isEmpty()){
+            throw new MoodAnalysisException(MoodAnalysisException.ExceptionType.EMPTY_MOOD, "Mood Cannot be Empty");
+        }
+        if (this.message.trim().toLowerCase().contains("sad")){
             return "SAD";
         }else {
             return "HAPPY";
         }
     }
+
     public static void main(String[] args) {
         MoodAnalyzer analyzer = new MoodAnalyzer("I am in Happy Mood");
 
-        String hAnalyse = analyzer.moodAnalyze();
-        System.out.println("Mood Is: " + hAnalyse);
+        try {
+            String hAnalyse = analyzer.moodAnalyze();
+            System.out.println("Mood Is: " + hAnalyse);
+        } catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
 
         MoodAnalyzer analyser = new MoodAnalyzer("I am in SAD mood");
 
-         String sAnalyse = analyser.moodAnalyze();
-        System.out.println("Mood Is: " + sAnalyse);
+        try {
+            String sAnalyse = analyser.moodAnalyze();
+            System.out.println("Mood Is: " + sAnalyse);
+        } catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
 
         MoodAnalyzer check = new MoodAnalyzer("I am Not in any mood");
-        String any = check.moodAnalyze();
-        System.out.println("Mood Is: " + any);
+
+        try {
+            String any = check.moodAnalyze();
+            System.out.println("Mood Is: " + any);
+        } catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
+
+        MoodAnalyzer nullCheck = new MoodAnalyzer(null);
+
+        try {
+            String nullMood = nullCheck.moodAnalyze();
+            System.out.println("Mood Is: " + nullMood);
+        } catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
+
+        MoodAnalyzer emptyCheck = new MoodAnalyzer("");
+
+        try {
+            String emptyMood = emptyCheck.moodAnalyze();
+            System.out.println("Mood Is: " + emptyMood);
+        } catch (MoodAnalysisException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+class MoodAnalysisException extends Exception {
+    enum ExceptionType {
+        NULL_MOOD, EMPTY_MOOD
+    }
+
+    private ExceptionType type;
+    public MoodAnalysisException(ExceptionType type, String message){
+        super(message);
+        this.type = type;
+    }
+
+    public ExceptionType getType(){
+        return this.type;
     }
 }
--- src/test/java/com/bl/TestingMood.java
+++ src/test/java/com/bl/TestingMood.java
@@ -1,48 +1,24 @@
-package com.bl;
-
-import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
-
-public class TestingMood {
-
-
-    //TC2.1
-
-    @Test
-    public void testAnalyseMoodForNull(){
-        ExceptionMood analyse = new ExceptionMood("My mood is Null");
-        String mood = analyse.analyseMood();
-        assertEquals("HAPPY", mood);
-        System.out.println(mood);
-    }
-
-    //TC1.1
-
-    @Test
-    public void testAnalyseMoodForSad() {
-        MoodAnalyzer analyser = new MoodAnalyzer("I am in Sad Mood");
-        String mood = analyser.moodAnalyze();
-        assertEquals("SAD", mood);
-        System.out.println(mood);
-    }
-
-    @Test
-    public void testAnalyseMoodForHappy() {
-        MoodAnalyzer analyser = new MoodAnalyzer("I am feeling happy today");
-        String mood = analyser.moodAnalyze();
-        assertEquals("HAPPY", mood);
-        System.out.println(mood);
-    }
-
-    //TC1.2
-
-    @Test
-    public void testAnalyseMoodForAny(){
-        MoodAnalyzer analyzer = new MoodAnalyzer("I am Not in any Mood");
-        String mood = analyzer.moodAnalyze();
-        assertEquals("HAPPY", mood);
-        System.out.println(mood);
-    }
-}
-
+//package com.bl;
+//
+//import org.junit.Test;
+//
+//import org.junit.jupiter.api.Assertions;
+//
+//public class TestingMood {
+//
+//    @Test
+//    public void SadMood() throws MoodAnalysisException {
+//        MoodAnalyzer analyzer = new MoodAnalyzer("I am in Sad Mood");
+//        String mood = analyzer.moodAnalyze();
+//        Assertions.assertEquals("SAD", mood);
+//    }
+//
+//    @Test
+//    public  void HappyMood() throws MoodAnalysisException {
+//        MoodAnalyzer analyzer = new MoodAnalyzer("Today I am So Happy");
+//        String mood = analyzer.moodAnalyze();
+//        Assertions.assertEquals("HAPPY", mood);
+//    }
+//
+//}
+//
