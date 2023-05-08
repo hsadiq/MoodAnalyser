8fd29fff65fbb283f96296dd828e2a465fa1a976
hsadiq <ksadik320@gmail.com>
2023-05-08 12:34:15 +0530

    Added Testing Case For Null Mood

--- src/test/java/com/bl/TestingMood.java
+++ src/test/java/com/bl/TestingMood.java
@@ -1,24 +1,35 @@
-//package com.bl;
-//
-//import org.junit.Test;
-//
-//import org.junit.jupiter.api.Assertions;
-//
-//public class TestingMood {
-//
-//    @Test
-//    public void SadMood() throws MoodAnalysisException {
-//        MoodAnalyzer analyzer = new MoodAnalyzer("I am in Sad Mood");
-//        String mood = analyzer.moodAnalyze();
-//        Assertions.assertEquals("SAD", mood);
-//    }
-//
-//    @Test
-//    public  void HappyMood() throws MoodAnalysisException {
-//        MoodAnalyzer analyzer = new MoodAnalyzer("Today I am So Happy");
-//        String mood = analyzer.moodAnalyze();
-//        Assertions.assertEquals("HAPPY", mood);
-//    }
-//
-//}
-//
+package com.bl;
+
+import org.junit.Test;
+
+import org.junit.jupiter.api.Assertions;
+
+public class TestingMood {
+
+    @Test
+    public void SadMood() throws MoodAnalysisException{
+        MoodAnalyzer analyzer = new MoodAnalyzer("I am in Sad Mood");
+        String mood = analyzer.moodAnalyze();
+        Assertions.assertEquals("SAD", mood);
+        System.out.println(mood);
+    }
+
+    @Test
+    public  void HappyMood() throws MoodAnalysisException {
+        MoodAnalyzer analyzer = new MoodAnalyzer("Today I am So Happy");
+        String mood = analyzer.moodAnalyze();
+        Assertions.assertEquals("HAPPY", mood);
+        System.out.println(mood);
+    }
+
+    @Test
+    public void NullMood(){
+        MoodAnalyzer analyzer = new MoodAnalyzer(null);
+        try {
+            analyzer.moodAnalyze();
+        }catch (MoodAnalysisException e){
+            Assertions.assertEquals(MoodAnalysisException.ExceptionType.NULL_MOOD, e.getType());
+        }
+    }
+}
+
